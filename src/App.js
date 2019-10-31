57385df205bbea4e5249f4b6e318474e706d3a4b
Peter Dang <Peter.x.Dang@gmail.com>
2019-10-31 09:56:41 -0500

    create input fields

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
+  
     <div className="App">
-     <h1>working</h1>
+     <h1>Pet Hotel </h1>
+     <input placeholder = "Pet Name"></input>
+     <input placeholder = "Pet Breed"></input>
+     <button>Submit</button>
     </div>
   );
 }
